f1ab43fc72989f9007ba419c75bf689c58cdf52a
GiuDe-2122110465 <giude.ltcpgd@gmail.com>
2026-03-12 08:50:38 +0700

    bai 1

--- src/components/MyComponents.jsx
+++ src/components/MyComponents.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 class MyComponent extends React.Component 
 {
+    state ={
+        name:'giu de',
+        age: 22,
+        address:'hcm',
+    }
+    handleClick(event)
+    {
+        alert('click cc')
+    }
+    handMouseOver(event)
+    {
+        console.log(event.pageX)
+    }
     render()
     {
         return (
-            <div>my first componnent</div>
+            <div>
+                my name is {this.state.name}
+                <button onClick={this.handleClick}>click vào đây</button>
+                <button onMouseOver={this.handMouseOver}>di chuột vào đây</button>
+
+            </div>
         );
 
     }
