f0d586e69b173613590ab45d7f0fdb9f87c2ebc2
GiuDe-2122110465 <giude.ltcpgd@gmail.com>
2026-03-12 22:22:50 +0700

    bai 3

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -3,12 +3,25 @@ import React from "react";
 class DisplayInfo extends React.Component {
  render ( )
  {
-    const {age,name} = this.props;
+    const {lisUser} = this.props;
+    console.log(lisUser)
     return (
         <div> 
-            <div>my name is {name}</div>
+            {lisUser.map((user)=>{
+                return (
+                    <div key={user.id}>
+                        <hr />
+                        <div>my name is {user.name}</div>
+                        <div>my age is {user.age}</div>
+                        <div>my Address is {user.address}</div>
+                        <hr />
+                    </div>
+                    
+                )
+            })}
+            {/* <div>my name is {name}</div>
             <div>my age is {age}</div>
-            <div>my Address is HCM</div>
+            <div>my Address is HCM</div> */}
 
         </div>
     )
--- src/components/MyComponents.jsx
+++ src/components/MyComponents.jsx
@@ -3,18 +3,26 @@ import UserInfo from "./UserInfo";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component 
 {
-   
+   state ={
+    lisUser:[
+            {id:1,name:'giu de',address:'HCM'},
+            {id:2,name:'giu de 1',address:'VN'},
+            {id:3,name:'giu de 2',address:'USA'}
+
+    ]
+   }
     render()
     {
+        //DRY
         return (
             <div>
                 
                
             <UserInfo></UserInfo>
-            <DisplayInfo name = " giu de 2" age = "222"></DisplayInfo>
-            <hr />
-            <DisplayInfo name = " giu de 2" age = {333}></DisplayInfo>
-
+    
+            <DisplayInfo 
+                lisUser={this.state.lisUser} 
+            />
             </div>
         );
 
