89b79124627ab787f69de266554e8cbb0830454f
GiuDe-2122110465 <giude.ltcpgd@gmail.com>
2026-03-12 23:07:55 +0700

    bai tiep theo

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+    state={
+        isShowListUser:true
+    }
+    HandleShowHide = ()=>{
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
  render ( )
  {
     const {lisUser} = this.props;
-    console.log(lisUser)
+    console.log("check map user",lisUser)
     return (
-        <div> 
-            {lisUser.map((user)=>{
-                return (
-                    <div key={user.id}>
-                        <hr />
-                        <div>my name is {user.name}</div>
-                        <div>my age is {user.age}</div>
-                        <div>my Address is {user.address}</div>
-                        <hr />
-                    </div>
-                    
-                )
-            })}
-            {/* <div>my name is {name}</div>
-            <div>my age is {age}</div>
-            <div>my Address is HCM</div> */}
+        <div>
+            <div>
+                <span onClick={()=>{this.HandleShowHide()}}>
+                    {(this.state.isShowListUser===true) ? 'ẩn user' : 'hiện user'}
+                </span>
+            </div>
+            {this.state.isShowListUser && 
+                <div> 
+                    {lisUser.map((user)=>{
+                        return (
+                            <div key={user.id} className={+user.age>18 ? "green" : "red"}>
+                                <hr />
+                                <div>my name is {user.name}</div>
+                                <div>my age is {user.age}</div>
+                                <hr />
+                            </div>
+                            
+                        )
+                    })}
+                    {/* <div>my name is {name}</div>
+                    <div>my age is {age}</div>
+                    <div>my Address is HCM</div> */}
 
+                </div>}
+            
         </div>
+        
     )
  }
 
--- src/components/MyComponents.jsx
+++ src/components/MyComponents.jsx
@@ -5,9 +5,13 @@ class MyComponent extends React.Component
 {
    state ={
     lisUser:[
-            {id:1,name:'giu de',address:'HCM'},
-            {id:2,name:'giu de 1',address:'VN'},
-            {id:3,name:'giu de 2',address:'USA'}
+            {id:1,name:'giu de',age: "15"},
+            {id:2,name:'giu de 1',age: "16"},
+            {id:3,name:'giu de 2',age: "17"},
+            {id:4,name:'giu de 3',age: "27"},
+            {id:5,name:'giu de 4',age: "26"},
+            {id:6,name:'giu de 5',age: "22"}
+
 
     ]
    }
