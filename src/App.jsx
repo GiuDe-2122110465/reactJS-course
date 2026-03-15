a0d45009d1aab92ec36e97caf562f6bc70950731
GiuDe-2122110465 <giude.ltcpgd@gmail.com>
2026-03-15 11:15:11 +0700

    bat dau project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ 
 import './App.scss'
-import MyComponent from './components/MyComponents'
-import React from 'react'
-class App extends React.Component
-{
-  render() {
-    return (
-    <div>
-    <MyComponent></MyComponent>
-
+ import Header from './components/Header/Header';
+const App =()=>{
+  return(
+    <div className='app-container'>
+      <Header/>
     </div>
-    )
-  }
-  
-  
+  );
 }
 
 export default App
